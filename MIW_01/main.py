c3e30f56aafdd0c71fbc5f5cc1b70dced5ad1b1a
Dmytro Brahinskyi <dbraginskiy821@gmail.com>
2026-03-16 21:58:02 +0100

    task1: add 4th degree approximation

--- MIW_01/main.py
+++ MIW_01/main.py
@@ -11,15 +11,14 @@ x = a[:,[0]]
 y = a[:,[1]]
 # print(y)
 
-x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.2)
+#LEVEL 3 POLYNOMIAL
 
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.2)
 # Building the matrix
 cTrain = np.hstack([x_train**3, x_train**2, x_train, np.ones(x_train.shape)])
-
 #Model training
 vTrain = np.linalg.pinv(cTrain) @ y_train
 # e = y_train - y_pred. Now calculating predicted to find e (error):
-
 #Predictions
 yPredTrain = vTrain[0]*x_train**3 + vTrain[1]*x_train**2 + vTrain[2]*x_train + vTrain[3]
 yPredTest = vTrain[0]*x_test**3 + vTrain[1]*x_test**2 + vTrain[2]*x_test + vTrain[3]
@@ -30,6 +29,24 @@ eTest = y_test - yPredTest
 print('Model 3 - polynomial homework')
 print("Train : ", (eTrain.T@eTrain)/len(eTrain))
 print("Test : ", (eTest.T@eTest)/len(eTest))
+
+
+#LEVEL 4 POLYNOMIAL
+c4Train = np.hstack([x_train**4, x_train**3, x_train**2, x_train, np.ones(x_train.shape)])
+v4Train = np.linalg.pinv(c4Train) @ y_train
+y4PredTrain = v4Train[0]*x_train**4 + v4Train[1]*x_train**3 + v4Train[2]*x_train**2 + v4Train[3]*x_train + v4Train[4]
+y4PredTest = v4Train[0]*x_test**4 + v4Train[1]*x_test**3 + v4Train[2]*x_test**2 + v4Train[3]*x_test + v4Train[4]
+
+e4Train = y_train - y4PredTrain
+e4Test = y_test - y4PredTest
+
+print('Model 4 - polynomial homework')
+print("Train : ", (e4Train.T@e4Train)/len(e4Train))
+print("Test : ", (e4Test.T@e4Test)/len(e4Test))
+
+
+
+
 
 c = np.hstack([x*x*x, x*x, x, np.ones(x.shape)])
 v = np.linalg.inv(c.T@c)@c.T @ y
@@ -61,10 +78,16 @@ plt.scatter(x_test, y_test, color = 'red', label = 'tesing data')
 x_line = np.linspace(min(x), max(x), 100)
 y_line = vTrain[0]*x_line**3 + vTrain[1]*x_line**2 + vTrain[2]*x_line + vTrain[3]
 
-plt.plot(x_line, y_line, color='orange', linewidth=2, label='polynomial approximation')
+y_line4 = v4Train[0]*x_line**4 + v4Train[1]*x_line**3 + v4Train[2]*x_line**2 + v4Train[3]*x_line + v4Train[4]
+
+plt.plot(x_line, y_line, color='orange', linewidth=2, label='3d degree polynomial approximation')
+plt.plot(x_line, y_line4, color='cyan', linewidth=2, label='4th degree polynomial approximation')
 
 plt.legend()
 plt.show()
+
+
+
 
 
 # plt.plot(x,v[0]*x*x*x + v[1]*x*x + v[2]*x + v[3],)
